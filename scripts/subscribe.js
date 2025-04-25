1661051df107c4dab5dfa25a65fc8cdaa15140ca
Lili Wang <clover1983@hotmail.com>
2025-04-25 01:52:56 -0400

    remve un-need comment

--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const subscribeForm = document.querySelector(".newsletter form");
   if (subscribeForm) {
     subscribeForm.addEventListener("submit", function (e) {
-      e.preventDefault(); // 防止表单实际提交
+      e.preventDefault();
       alert("Thank you for subscribing.");
-      subscribeForm.reset(); // 可选：提交后清空输入框
+      subscribeForm.reset();
     });
   }
 });
